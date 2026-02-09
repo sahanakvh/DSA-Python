a29c1997a0a4ecc847f8b25c6fa649f38c86bbc1
Sahana K <sahanakvh@gmail.com>
2026-02-09 23:21:33 +0530

    Added vowel and consonant count string problem

---
+++ Strings/count_vowels_consonants.py
@@ -0,0 +1,13 @@
+# Count vowels and consonants in a string
+text = "hello world"
+vowels = "aeiouAEIOU"
+vowel_count = 0
+consonant_count = 0
+for char in text:
+    if char.isalpha():
+        if char in vowels:
+            vowel_count += 1
+        else:
+            consonant_count += 1
+print("Vowels:", vowel_count)
+print("Consonants:", consonant_count)
