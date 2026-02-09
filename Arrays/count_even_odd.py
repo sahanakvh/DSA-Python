62a4911d2c7672277eb1524460f32a6c1cf199dc
Sahana K <sahanakvh@gmail.com>
2026-02-09 21:32:23 +0530

    Added even and odd count array problem

---
+++ Arrays/count_even_odd.py
@@ -0,0 +1,11 @@
+# Count even and odd elements in an array
+arr = [1, 2, 3, 4, 5, 6]
+even = 0
+odd = 0
+for num in arr:
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print("Even count:", even)
+print("Odd count:", odd)
